d0e455ad6c7d98644019f54c981fb3d95bbd6876
denvudd <dimontv7@gmail.com>
2024-05-10 20:01:06 +0300

    init: craco

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Admin, DataProvider, Loading, Resource } from "react-admin";
 import buildHasuraProvider from "ra-data-hasura";
 import { CssBaseline } from "@mui/material";
 
-import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
-import { MenuEdit } from "./modules/menu/components/menu-edit/menu-edit.component";
-import { MenuCreate } from "./modules/menu/components/menu-create/menu-create.component";
+import { MenuList } from "@/modules/menu/components/menu-list/menu-list.component";
+import { MenuEdit } from "@/modules/menu/components/menu-edit/menu-edit.component";
+import { MenuCreate } from "@/modules/menu/components/menu-create/menu-create.component";
 
 function App() {
   const [dataProvider, setDataProvider] = React.useState<DataProvider | null>(
