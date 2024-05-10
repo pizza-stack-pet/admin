6e3628013e0cb39e3df7a3dcb7acaf685eb1e3ff
denvudd <dimontv7@gmail.com>
2024-05-10 10:49:06 +0300

    feat: crud for menu list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import React from "react";
+import { Admin, DataProvider, Loading, Resource } from "react-admin";
+import buildHasuraProvider from "ra-data-hasura";
+import { CssBaseline } from "@mui/material";
+
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
+import { MenuEdit } from "./modules/menu/components/menu-edit/menu-edit.component";
+import { MenuCreate } from "./modules/menu/components/menu-create/menu-create.component";
+
 function App() {
+  const [dataProvider, setDataProvider] = React.useState<DataProvider | null>(
+    null
+  );
+
+  React.useEffect(() => {
+    const buildDataProvider = async () => {
+      const dataProvider = await buildHasuraProvider({
+        clientOptions: { uri: "http://localhost:8080/v1/graphql" },
+      });
+
+      setDataProvider(() => dataProvider);
+    };
+
+    buildDataProvider();
+  }, []);
+
+  if (!dataProvider) {
+    return <Loading />;
+  }
+
   return (
-    <h1>Pizza Stack</h1>
+    <>
+      <CssBaseline />
+      <Admin dataProvider={dataProvider}>
+        <Resource name="menu" list={MenuList} edit={MenuEdit} create={MenuCreate} />
+      </Admin>
+    </>
   );
 }
 
